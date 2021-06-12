e8eaadb9d2022d1f1604342aa4fada12fb42671a
joao <bairrada1997@gmail.com>
2021-06-12 18:21:25 +0100

    add slide effect to tabs

--- components/MatchTabsWrapper/MatchTabsWrapper.vue
+++ components/MatchTabsWrapper/MatchTabsWrapper.vue
@@ -11,12 +11,22 @@
 </template>
 
 <script>
-    import { defineComponent, provide, toRefs, ref, onMounted, useFetch, onActivated, computed, watch } from "@nuxtjs/composition-api";
+    import { defineComponent, provide, reactive, ref, onMounted, useFetch, onActivated, computed, watch } from "@nuxtjs/composition-api";
     import store from "@/store.js";
     import axios from "axios";
 
     export default defineComponent({
         setup(props, { slots }) {
+            const state = reactive({
+                initialX: null,
+                initialY: null,
+                currentX: 0,
+                currentY: 0,
+                diffX: 0,
+                diffY: 0,
+                isDragging: false,
+                isStartTouch: false
+            });
             const matchDetail = ref("");
             const tabTitles = ref({});
             const selectedTab = ref({});
@@ -25,6 +35,16 @@
                 tabTitles.value = tabTitles.value.filter(item => !!item);
 
                 selectedTab.value = tabTitles.value[0];
+
+                const body = document.querySelector("body");
+                body.addEventListener("touchstart", startTouch, { passive: false });
+                body.addEventListener("mousedown", startTouch, { passive: false });
+
+                body.addEventListener("touchmove", moveTouch, { passive: false });
+                body.addEventListener("mousemove", moveTouch, { passive: false });
+
+                body.addEventListener("mouseup", touchEnd, { passive: false });
+                body.addEventListener("touchend", touchEnd, { passive: false });
             });
 
             provide("selectedTab", selectedTab);
@@ -36,9 +56,73 @@
                 const tabs = matchDetail.value.querySelectorAll(".matchTabsWrapper__tab");
                 const selectedTabIndex = [...tabs].findIndex((element, index) => element.innerText.toLowerCase() == selectedTab.value.toLowerCase());
 
-                if (selectedTabIndex == 0 || selectedTabIndex == tabs.length - 1 || selectedTabIndex == tabs.length - 2) return;
-                tabsList.style.transform = `translateX(${-tabsListWidth + 94 * (tabs.length + 1 - selectedTabIndex)}px)`;
+                if (selectedTabIndex == 0 || selectedTabIndex == tabs.length - 1) return;
+                if (selectedTabIndex == tabs.length - 3 || selectedTabIndex == tabs.length - 2) {
+                    tabsList.style.transform = `translateX(${-tabsListWidth + 94 * 4 - 16}px)`;
+                } else {
+                    tabsList.style.transform = `translateX(${-tabsListWidth + 94 * (tabs.length - selectedTabIndex + 1)}px)`;
+                }
+
+                state.diffX = 0;
             };
+
+            const userNotStartedDrag = () => state.initialX === null || state.initialY === null || !state.isStartTouch;
+            const userSlideYCoordinate = () => Math.abs(state.diffX) < Math.abs(state.diffY);
+            const userSlideToRight = () => state.diffX > 0;
+
+            const setSliderValues = event => {
+                state.isDragging = true;
+                state.currentX = event instanceof MouseEvent ? event.clientX : event.touches[0].clientX;
+                state.currentY = event instanceof MouseEvent ? event.clientY : event.touches[0].clientY;
+                state.diffX = state.initialX - state.currentX;
+                state.diffY = state.initialY - state.currentY;
+            };
+
+            const startTouch = event => {
+                const isTargetTabs = event => event.target.closest(".matchTabsWrapper__list");
+                if (!isTargetTabs(event)) return;
+
+                state.initialX = event instanceof MouseEvent ? (state.diffX += event.clientX) : (state.diffX += event.touches[0].clientX);
+                state.initialY = event instanceof MouseEvent ? event.clientY : event.touches[0].clientY;
+                state.isStartTouch = true;
+            };
+            const moveTouch = event => {
+                const tabsList = matchDetail.value.querySelector(".matchTabsWrapper__list");
+                if (userNotStartedDrag()) return;
+                setSliderValues(event);
+                translateCalendarOnSwipe(tabsList);
+                event.preventDefault();
+            };
+
+            const translateCalendarOnSwipe = tabs => {
+                if (userSlideYCoordinate()) return;
+                const tabsList = matchDetail.value.querySelector(".matchTabsWrapper__list");
+                tabsList.style.transition = "none";
+                const tabsElements = tabs.querySelectorAll(".matchTabsWrapper__tab");
+
+                if (state.diffX > 204) {
+                    tabs.style.transform = `translate3d(${-tabs.scrollWidth + 94 * 4 - 16}px, 0, 0) `;
+                    return;
+                }
+
+                if (state.diffX < 0) {
+                    tabs.style.transform = `translate3d(0px, 0, 0) `;
+                    return;
+                }
+                userSlideToRight() ? (tabs.style.transform = `translate3d(-${state.diffX}px, 0, 0) `) : (tabs.style.transform = `translate3d(${-state.diffX}px, 0, 0) `);
+            };
+
+            const touchEnd = () => {
+                state.isStartTouch = false;
+                if (!state.isDragging) return;
+                if (userSlideYCoordinate()) return;
+                const tabsList = matchDetail.value.querySelector(".matchTabsWrapper__list");
+                tabsList.style.transition = "transform 0.3s ease";
+                //userSlideToRight() ? goNext() : goPrev();
+
+                state.isDragging = false;
+            };
+
             return {
                 tabTitles,
                 selectedTab,
